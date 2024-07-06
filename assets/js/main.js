f064fdcaea30f00a2748d3cf83f5a6807f9817eb
Trailblazer-dev <richvictor830@gmail.com>
2024-07-06 21:28:31 +0300

    creating

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,28 @@ window.addEventListener('scroll',scrollHeader);
 
 
 /*=============== SERVICES MODAL ===============*/
-
-
+const modalViews= document.querySelectorAll('.services__modal'),
+modalBtns=document.querySelectorAll('.services__button'),
+modalClose= document.querySelectorAll('.services__modal-close');
+let modal= (modalClick)=>{
+    modalViews[modalClick].classList.add('active-modal');
+}
+modalBtns.forEach((mb,i)=>{
+    mb.addEventListener('click',()=>{
+        modal(i);
+    })
+});
+// let close =(closeClick)=>{
+//     modalViews[closeClick].classList.remove('active-modal');
+// }
+// modalClose.forEach((mb,i)=>{mb.addEventListener('click',()=>{
+//     close(i);
+// })});
+modalClose.forEach((mc)=>{
+    mc.addEventListener('click',()=>{
+        modalViews.forEach((mv)=>{mv.classList.remove('active-modal');})
+    })
+})
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
